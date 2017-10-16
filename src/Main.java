b36cd4602f7b1b347ac8b0b27254e97b08bed696
Jeremy School <jschool@catalyte.io>
2017-10-16 18:13:36 -0400

    input from user

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,25 @@
  * and has a method to generate a description paragraph about them.
  */
 
+import java.util.Scanner;
+
 public class Main {
 	// <scope> <return_type> <method_name>
 	public static void main(String[] args) {
-		Info johnsInfo = new Info("John", "Squarepants", 32);
+		Scanner scanner = new Scanner(System.in); // System.in takes in data from the terminal
+		
+		System.out.print("Please enter first name: "); // Acts as a prompt, specifying to the user what type of data we need
+		String firstName = scanner.next(); //  causes program to wait for input, then stores the input in firstName as a String
+		
+		System.out.print("Please enter last name: ");
+		String lastName = scanner.next();
+		
+		System.out.print("Please enter age: ");
+		int age = scanner.nextInt();
+
+		Info johnsInfo = new Info(firstName, lastName, age);
 		System.out.println(johnsInfo.getBio());
+		
+		scanner.close();
 	}
 }
